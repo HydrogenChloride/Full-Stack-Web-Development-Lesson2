6720be77e47da8f54aef6594184771b70489a651
JP Pasion <johnpaulpasionbricks01@gmail.com>
2022-09-13 23:19:59 +0800

    React Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
 import { DISHES } from "./shared/dishes";
+import { BrowserRouter } from 'react-router-dom';
+
 
 class App extends Component {
   constructor(props){
@@ -13,9 +15,12 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+      
     );
   }
 }
