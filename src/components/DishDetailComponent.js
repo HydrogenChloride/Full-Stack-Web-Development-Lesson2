2eed6f90ee3ec3e5fd8d1a2ec05caaedc6bd76db
JP Pasion <johnpaulpasionbricks01@gmail.com>
2022-09-17 19:25:34 +0800

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -13,7 +14,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
             return (
                     <div key={dish.id}>
                         <Card>
-                            <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                             <CardBody>
                                 <CardTitle><h4>{dish.name}</h4></CardTitle>
                                 <CardText>{dish.description}</CardText>
